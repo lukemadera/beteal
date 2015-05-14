25feff62d0d1d46b65c730ad157eba2aed6ee429
Luke Madera <luke.madera@gmail.com>
2015-05-14 13:21:44 -0700

    feat(orgs): modularize files, add geo file for getting location bounds based on lat, lng, and radius input

---
+++ organization/orgs/orgs-geo.js
@@ -0,0 +1,69 @@
+orgsObj ={};
+
+/**
+http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates
+http://stackoverflow.com/questions/1689096/calculating-bounding-box-a-certain-distance-away-from-a-lat-long-coordinate-in-j
+
+http://www.movable-type.co.uk/scripts/latlong.html
+http://www.movable-type.co.uk/scripts/latlong-vectors.html
+http://stackoverflow.com/questions/3939433/determine-points-within-a-given-radius-algorithm
+http://stackoverflow.com/questions/1185408/converting-from-longitude-latitude-to-cartesian-coordinates
+http://stackoverflow.com/questions/28727155/algorithm-mysql-get-all-points-within-a-radius
+*/
+
+  /**
+  @param {Number} radius Distance from lat, lng in kilometers
+  @param {Number} lat Latitude in degrees
+  @param {Number} lng Longitude in degrees
+  */
+  orgsObj.computeBoundingLatLng =function(radius, lat, lng, params) {
+    var ret ={};
+
+    var latRadians =this.degreesToRadians(lat);
+    var lngRadians =this.degreesToRadians(lng);
+
+    var earthRadius =6371;    //km (NOT miles)
+    var MIN_LAT =-1 *(Math.PI /2);
+    var MAX_LAT =Math.PI /2;
+    var MIN_LNG =-1 *(Math.PI);
+    var MAX_LNG =Math.PI;
+
+    var radDist =radius /earthRadius;
+
+    ret.latMin =latRadians -radDist;
+    ret.latMax =latRadians +radDist;
+
+    if(ret.latMin > MIN_LAT && ret.latMax < MAX_LAT) {
+      var lngDelta =Math.asin(Math.sin(radDist) / Math.cos(radLat));
+      ret.lngMin =lngRadians - lngDelta;
+      if(ret.lngMin < MIN_LNG) {
+        ret.lngMin += 2 * Math.PI;
+      }
+      ret.lngMax = lngRadians + lngDelta;
+      if(ret.lngMax > MAX_LNG) {
+        ret.lngMax -= 2 * Math.PI;
+      }
+    }
+    else {
+      // a pole is within the distance
+      ret.latMin = Math.max(ret.latMin, MIN_LAT);
+      ret.latMax = Math.min(ret.latMax, MAX_LAT);
+      ret.lngMin = MIN_LNG;
+      ret.lngMax = MAX_LNG;
+    }
+
+    //convert to back to degrees
+    ret.latMin =this.radiansToDegrees(ret.latMin);
+    ret.latMax =this.radiansToDegrees(ret.latMax);
+    ret.lngMin =this.radiansToDegrees(ret.lngMin);
+    ret.lngMax =this.radiansToDegrees(ret.lngMax);
+    return ret;
+  };
+
+  orgsObj.radiansToDegrees =function(radians) {
+    return radians * (180 / Math.PI);
+  };
+
+  orgsObj.degreesToRadians =function(degrees) {
+    return degrees * (Math.PI / 180);
+  };
---
+++ organization/orgs/orgs-helpers.js
@@ -0,0 +1,109 @@
+// orgsObj ={};
+
+if(Meteor.isClient) {
+
+  orgsObj.getMainTemplate =function(params) {
+    var view =Blaze.currentView;
+    if(view.name !=="Template.orgs") {
+      //get parent template instance if not on correct one - http://stackoverflow.com/questions/27949407/how-to-get-the-parent-template-instance-of-the-current-template
+      while (view && view.name !=="Template.orgs") {
+        view = view.parentView;
+      }
+    }
+    return view.templateInstance();
+  };
+
+  orgsObj.getOrgs =function(query, params) {
+    var templateInst =this.getMainTemplate({});
+    var orgs =OrganizationsCollection.find(query).fetch();
+    var ii;
+    for(ii =0; ii<orgs.length; ii++) {
+      orgs[ii].xDisplay ={
+        location: orgs[ii].locations[0]
+      };
+    }
+    console.log('query: ', query, 'orgs: ', orgs);
+    return orgs;
+  };
+
+  orgsObj.formQuery =function(params) {
+    var query ={};
+    var templateInst =this.getMainTemplate({});
+    var filters =templateInst.filters.get();
+    var ii;
+    for(ii =0; ii<filters.length; ii++) {
+      if(filters[ii].active) {
+        if(filters[ii].key ==='name') {
+          query.name ={
+            $regex: filters[ii].val,
+            $options: 'i'
+          }
+        }
+      }
+    }
+    return query;
+  };
+
+  orgsObj.search =function(params) {
+    var query =this.formQuery({});
+    var orgs =this.getOrgs(query, {});
+    var templateInst =this.getMainTemplate({});
+    templateInst.orgs.set(orgs);
+  };
+
+  orgsObj.initFilters =function(params) {
+    var filters =[
+      {
+        template: 'orgsFilterName',
+        key: 'name',
+        val: ''
+      }
+    ];
+    var ii;
+    for(ii =0; ii<filters.length; ii++) {
+      filters[ii].active =false;
+      filters[ii].visible =false;
+    }
+    return filters;
+  };
+
+  orgsObj.setFilter =function(key, val, params) {
+    var templateInst =this.getMainTemplate({});
+    var filters =templateInst.filters.get();
+    var index1 =nrArray.findArrayIndex(filters, 'key', key, {});
+    filters[index1].val =val;
+    filters[index1].active =true;
+    filters[index1].visible =true;
+    templateInst.filters.set(filters);
+  };
+
+  orgsObj.unsetFilter =function(key, params) {
+    var templateInst =this.getMainTemplate({});
+    var filters =templateInst.filters.get();
+    var index1 =nrArray.findArrayIndex(filters, 'key', key, {});
+    // filters[index1].val ='';
+    filters[index1].active =false;
+    var showFiltersInactive =templateInst.showFiltersInactive.get();
+    if(!showFiltersInactive) {
+      filters[index1].visible =false;
+    }
+    templateInst.filters.set(filters);
+  };
+
+  orgsObj.toggleShowInactiveFilters =function(params) {
+    var templateInst =this.getMainTemplate({});
+    var visible =templateInst.showFiltersInactive.get();
+    visible =!visible;
+
+    var filters =templateInst.filters.get();
+    var ii;
+    for(ii =0; ii<filters.length; ii++) {
+      if(!filters[ii].active) {
+        filters[ii].visible =visible;
+      }
+    }
+
+    templateInst.filters.set(filters);
+    templateInst.showFiltersInactive.set(visible);
+  };
+}
